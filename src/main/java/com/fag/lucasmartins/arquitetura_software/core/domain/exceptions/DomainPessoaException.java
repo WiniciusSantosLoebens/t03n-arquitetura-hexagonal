8f18a65151c6760c3fea7036f437cbf3e6268001
WiniciusSantosLoebens <wsloebens@minha.fag.edu.br>
2026-04-13 20:31:12 -0300

    Delete src/main/java/com/fag/lucasmartins/arquitetura_software/core/domain/exceptions/DomainPessoaException.java

--- src/main/java/com/fag/lucasmartins/arquitetura_software/core/domain/exceptions/DomainPessoaException.java
+++
@@ -1,8 +0,0 @@
-package com.fag.lucasmartins.arquitetura_software.core.domain.exceptions;
-
-public class DomainPessoaException extends RuntimeException {
-
-    public DomainPessoaException(String message) {
-        super(message);
-    }
-}
